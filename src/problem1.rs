5259c1635e3860504bede30e4f831bf97f8a1f57
Zbigniew Siciarz <antyqjon@gmail.com>
2014-05-13 21:37:35 +0200

    Problem 1 is more readable without temporary variable.

--- src/problem1.rs
+++ src/problem1.rs
@@ -1,5 +1,4 @@
 pub fn solution() -> int {
     use std::iter::AdditiveIterator;
-    let mut numbers = range(1, 1000).filter(|i| i % 3 == 0 || i % 5 == 0);
-    numbers.sum()
+    range(1, 1000).filter(|i| i % 3 == 0 || i % 5 == 0).sum()
 }
