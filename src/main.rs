790a26bd24b01116bce5e47075ec17cfc844fb31
Zbigniew Siciarz <antyqjon@gmail.com>
2014-06-15 14:22:22 +0200

    Read problem number from commandline args.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 mod solutions;
 
 fn main() {
+    let args = std::os::args();
+    match args.as_slice() {
+        [_, ref solution, ..] => println!("{}", solution),
+        [ref program_name] => println!("Usage: {} <PROBLEM NUMBER>", program_name),
+    };
     println!("{}", solutions::solution(14));
 }
