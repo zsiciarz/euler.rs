fa16970dc7d50e426da668541ebb2be03a38266e
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-08 22:26:21 +0200

    Added prime_factor_groups function.

--- src/solutions/common.rs
+++ src/solutions/common.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn prime_factors(n: int) -> Vec<int> {
     let mut factors = Vec::new();
     let mut q = n;
@@ -15,4 +17,13 @@ pub fn prime_factors(n: int) -> Vec<int> {
         factors.push(q);
     }
     factors
+}
+
+pub fn prime_factor_groups(n: int) -> HashMap<int, int> {
+    let mut powers = HashMap::new();
+    let factors = prime_factors(n);
+    for factor in factors.move_iter() {
+        powers.insert_or_update_with(factor, 1i, |k: &int, v: &mut int| *v += 1);
+    }
+    powers
 }
