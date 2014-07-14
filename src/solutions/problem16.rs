8ee51e53bdc58b119b180d9490bd64a1e87ec8d8
Zbigniew Siciarz <antyqjon@gmail.com>
2014-07-14 12:44:25 +0200

    Use from_int in 16.

--- src/solutions/problem16.rs
+++ src/solutions/problem16.rs
@@ -1,12 +1,12 @@
-use std::num::pow;
+use std::num::{from_int,pow};
 use num::BigInt;
 
 use super::common::digits;
 
 pub fn solution() -> int {
     use std::iter::AdditiveIterator;
-    let two = from_str::<BigInt>("2").unwrap();
-    match digits(pow(two, 1000)).move_iter().sum().to_int() {
+    let base: BigInt = from_int(2).unwrap();
+    match digits(pow(base, 1000)).move_iter().sum().to_int() {
         Some(x) => x,
         None => -1
     }
