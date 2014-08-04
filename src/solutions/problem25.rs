1b308afb6a071956da288843a23eea27f0ab9e3c
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-04 23:36:39 +0200

    This doesn't have to be mutable.

--- src/solutions/problem25.rs
+++ src/solutions/problem25.rs
@@ -5,6 +5,6 @@ use super::{SolutionResult};
 use super::common::{digits, Fib};
 
 pub fn solution() -> SolutionResult {
-    let mut fib: Fib<BigInt> = Fib::new();
+    let fib: Fib<BigInt> = Fib::new();
     Ok(fib.map(|i| digits(i).len()).take_while(|&i| i < 1000).count() as int)
 }
