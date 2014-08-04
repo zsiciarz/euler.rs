9db6a928d2d1f53029f8a4b607a52635bc6723d9
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-04 23:13:47 +0200

    Moved Fibonacci generator to common.rs

--- src/solutions/common.rs
+++ src/solutions/common.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::iter::MultiplicativeIterator;
+use std::mem;
 use std::num;
 use num::integer::Integer;
 
@@ -45,4 +46,25 @@ pub fn prime_factor_groups(n: int) -> HashMap<int, int> {
 
 pub fn num_divisors(n: int) -> int {
     prime_factor_groups(n).values().map(|&x| x + 1).product()
+}
+
+/// An infinite generator of Fibonacci sequence.
+pub struct Fib<T> {
+    prev: T,
+    current: T,
+}
+
+impl<T: Integer> Fib<T> {
+    pub fn new() -> Fib<T> {
+        Fib {prev: num::one(), current: num::one()}
+    }
+}
+
+impl<T: Integer> Iterator<T> for Fib<T> {
+    /// Returns next value from the Fibonacci sequence.
+    fn next(&mut self) -> Option<T> {
+        let current = self.prev + self.current;
+        let prev = mem::replace(&mut self.prev, current);
+        Some(mem::replace(&mut self.current, prev))
+    }
 }
--- src/solutions/problem2.rs
+++ src/solutions/problem2.rs
@@ -1,30 +1,8 @@
 use std::iter::AdditiveIterator;
-use std::mem;
-use std::num;
-use num::integer::Integer;
 
 use super::SolutionResult;
+use super::common::Fib;
 
-/// An infinite generator of Fibonacci sequence.
-struct Fib<T> {
-    prev: T,
-    current: T,
-}
-
-impl<T: Integer> Fib<T> {
-    fn new() -> Fib<T> {
-        Fib {prev: num::one(), current: num::one()}
-    }
-}
-
-impl<T: Integer> Iterator<T> for Fib<T> {
-    /// Returns next value from the Fibonacci sequence.
-    fn next(&mut self) -> Option<T> {
-        let current = self.prev + self.current;
-        let prev = mem::replace(&mut self.prev, current);
-        Some(mem::replace(&mut self.current, prev))
-    }
-}
 
 pub fn solution() -> SolutionResult {
     let fib: Fib<int> = Fib::new();
