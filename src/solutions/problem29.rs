6b8db7daae6841ecc424a1c3c87decdd81aac770
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-19 18:12:50 +0200

    Let's use BigUint instead of BigInt.

--- src/solutions/problem29.rs
+++ src/solutions/problem29.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
 use std::num;
-use num::BigInt;
+use num::BigUint;
 
 use super::{SolutionResult};
 
@@ -8,7 +8,7 @@ pub fn solution() -> SolutionResult {
     let mut s = HashSet::new();
     for a in range(2u, 101) {
         for b in range(2u, 101) {
-            let a: BigInt = num::from_uint(a).unwrap();
+            let a: BigUint = num::from_uint(a).unwrap();
             s.insert(num::pow(a, b));
         }
     }
