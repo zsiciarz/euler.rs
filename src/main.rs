20ce2b0444a9a723f16ec3b66246f2e2b47852a9
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-19 17:41:20 +0200

    Use getopts for commandline argument parsing.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 extern crate num;
+extern crate getopts;
 extern crate slow_primes;
 
+use getopts::{optopt, getopts};
 use solutions::{NotImplemented, SolutionNotFound, MatchFailed};
 
 mod solutions;
 
 fn main() {
     let args = std::os::args();
-    match args.as_slice() {
-        [ref program_name] => println!("Usage: {} <PROBLEM NUMBER>", program_name),
-        [_, ref problem, ..] => match from_str::<int>(problem.as_slice()) {
-            Some(problem_number) => match solutions::solution(problem_number) {
-                Ok(x) => println!("{}", x),
-                Err(e) => match e {
-                    NotImplemented => println!("Not implemented yet"),
-                    SolutionNotFound => println!("Solution not found"),
-                    MatchFailed => println!("Pattern matching failed"),
-                }
-            },
-            None => println!("Invalid problem number"),
+    let program_name = args[0].clone();
+    let opts = [
+        optopt("p", "problem", "Problem number", "PROBLEM NUMBER"),
+    ];
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => { m },
+        Err(f) => { fail!(f.to_string()) }
+    };
+    let problem_number = match matches.opt_str("p") {
+        Some(p) => { p },
+        None => {
+            println!("Usage: {} <PROBLEM NUMBER>", program_name);
+            return;
+        }
+    };
+    match from_str::<int>(problem_number.as_slice()) {
+        Some(problem_number) => match solutions::solution(problem_number) {
+            Ok(x) => println!("{}", x),
+            Err(e) => match e {
+                NotImplemented => println!("Not implemented yet"),
+                SolutionNotFound => println!("Solution not found"),
+                MatchFailed => println!("Pattern matching failed"),
+            }
         },
-        _ => unreachable!(),
+        None => println!("Invalid problem number")
     };
 }
