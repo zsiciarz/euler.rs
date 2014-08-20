f1fa641763ada415f98d6cf52c75da3711992d28
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-20 22:36:34 +0200

    Fleshed out 22.

--- src/solutions/problem22.rs
+++ src/solutions/problem22.rs
@@ -1,10 +1,20 @@
-use std::io::{BufferedReader, File};
+use std::io::File;
+use std::iter::AdditiveIterator;
 use std::path::Path;
 use super::{SolutionResult};
+
+fn name_value(name: &str) -> uint {
+    name.chars().filter_map(|c| if c == '\"' {
+        None
+    } else {
+        Some((c.to_ascii().to_byte() - 64u8) as uint)
+    }).sum()
+}
 
 pub fn solution() -> SolutionResult {
     let path = Path::new("data/p022_names.txt");
     let contents = File::open(&path).read_to_string().ok().expect("Cannot read file");
-    let names: Vec<&str> = contents.as_slice().split(',').collect();
-    Ok(22)
+    let mut names: Vec<&str> = contents.as_slice().split(',').collect();
+    names.sort();
+    Ok(names.move_iter().enumerate().map(|(i, name)| (i + 1) * name_value(name)).sum() as int)
 }
