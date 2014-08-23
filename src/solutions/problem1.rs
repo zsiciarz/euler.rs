fba554990898a113f079c521863738d76c712640
Zbigniew Siciarz <antyqjon@gmail.com>
2014-08-23 18:47:12 +0200

    Added simple test for last digits of solution to 1.

--- src/solutions/problem1.rs
+++ src/solutions/problem1.rs
@@ -5,3 +5,11 @@ use super::SolutionResult;
 pub fn solution() -> SolutionResult {
     Ok(range(1i, 1000).filter(|i| i % 3 == 0 || i % 5 == 0).sum())
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_solution() {
+        assert_eq!(super::solution().map(|s| s % 100i), Ok(68));
+    }
+}
