2f8b959a4780834e8f7026904e854bf8b15073f9
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-03 14:04:25 +0200

    Unwrap prime factors in solution for 3.

--- src/solutions/problem3.rs
+++ src/solutions/problem3.rs
@@ -2,7 +2,7 @@ use super::{SolutionResult, MatchFailed};
 use slow_primes::Primes;
 
 pub fn solution() -> SolutionResult {
-    match Primes::sieve(10000).factor(600851475143).iter().last() {
+    match Primes::sieve(10000).factor(600851475143).unwrap().iter().last() {
         None => Err(MatchFailed),
         Some(&(p, _)) => Ok(p as int)
     }
