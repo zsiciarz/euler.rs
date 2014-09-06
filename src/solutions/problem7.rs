74ecee4719c15f6fa9a3469f9675fb311abb31bf
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-06 14:42:41 +0200

    Use estimate_nth_prime as upper bound in 7.

--- src/solutions/problem7.rs
+++ src/solutions/problem7.rs
@@ -1,8 +1,10 @@
 use super::{SolutionResult, MatchFailed};
+use slow_primes;
 use slow_primes::Primes;
 
 pub fn solution() -> SolutionResult {
-    let primes = Primes::sieve(1000000);
+    let (_, upper_bound) = slow_primes::estimate_nth_prime(10001);
+    let primes = Primes::sieve(upper_bound as uint);
     match primes.primes().take(10001).last() {
         Some(p) => Ok(p as int),
         None => Err(MatchFailed),
