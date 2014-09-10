8022ebd22fef5660b2e1aa4be52d131818cd8fab
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-10 16:19:31 +0200

    Added result check to 18.

--- src/solutions/problem18.rs
+++ src/solutions/problem18.rs
@@ -20,3 +20,11 @@ pub fn solution() -> SolutionResult {
     }
     Ok(rows[0][0])
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_solution() {
+        assert_eq!(super::solution().map(|s| s % 100i), Ok(74));
+    }
+}
