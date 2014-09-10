95a49934710dd2a2d93e152236425a921ae72589
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-10 15:08:32 +0200

    Use move_iter to avoid reference in match.

--- src/solutions/problem3.rs
+++ src/solutions/problem3.rs
@@ -2,9 +2,9 @@ use super::{SolutionResult, MatchFailed};
 use slow_primes::Primes;
 
 pub fn solution() -> SolutionResult {
-    match Primes::sieve(10000).factor(600851475143).unwrap().iter().last() {
+    match Primes::sieve(10000).factor(600851475143).unwrap().move_iter().last() {
         None => Err(MatchFailed),
-        Some(&(p, _)) => Ok(p as int)
+        Some((p, _)) => Ok(p as int)
     }
 }
 
