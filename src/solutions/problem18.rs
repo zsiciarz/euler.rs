1de3518cf690c3a23373ab454935e395087cfd1f
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-10 15:55:11 +0200

    Working algorithm for 18 (toy data).

--- src/solutions/problem18.rs
+++ src/solutions/problem18.rs
@@ -1,5 +1,17 @@
+use std::cmp;
+
 use super::SolutionResult;
 
 pub fn solution() -> SolutionResult {
-    Ok(18)
+    let mut rows = Vec::new();
+    rows.push(vec![3i, 0, 0, 0]);
+    rows.push(vec![7i, 4, 0, 0]);
+    rows.push(vec![2i, 4, 6, 0]);
+    rows.push(vec![8i, 5, 9, 3]);
+    for i in range(0u, rows.len() - 1).rev() {
+        for j in range(0u, i + 1) {
+            *rows.get_mut(i).get_mut(j) += cmp::max(rows[i + 1][j], rows[i + 1][j + 1]);
+        }
+    }
+    Ok(rows[0][0])
 }
