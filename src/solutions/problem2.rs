bb18467d0415a6f86609ffd3c618e0914efca2a1
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-10 15:06:41 +0200

    Removed excessive newline.

--- src/solutions/problem2.rs
+++ src/solutions/problem2.rs
@@ -3,7 +3,6 @@ use std::iter::AdditiveIterator;
 use super::SolutionResult;
 use super::common::Fib;
 
-
 pub fn solution() -> SolutionResult {
     let fib: Fib<int> = Fib::new();
     Ok(fib.take_while(|&i| i <= 4000000).filter(|&i| i % 2 == 0).sum())
