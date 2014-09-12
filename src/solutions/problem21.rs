3b9676511cb76bbbebf39477c22f135fa9432b37
Zbigniew Siciarz <antyqjon@gmail.com>
2014-09-12 21:10:35 +0200

    Added divisors algorithm to 21.

--- src/solutions/problem21.rs
+++ src/solutions/problem21.rs
@@ -1,5 +1,19 @@
 use super::SolutionResult;
 
+/// Finds all proper divisors of an integer.
+fn divisors(n: int) -> Vec<int> {
+    let limit = (n as f32).sqrt().ceil() as int;
+    let mut divisors = Vec::new();
+    divisors.push(1);
+    for i in range(2i, limit) {
+        if n % i == 0 {
+            divisors.push(i);
+            divisors.push(n / i);
+        }
+    }
+    divisors
+}
+
 pub fn solution() -> SolutionResult {
     Ok(21)
 }
