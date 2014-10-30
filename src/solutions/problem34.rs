be351d1fbe73d7703ac63c05be47c797704e42d8
Zbigniew Siciarz <antyqjon@gmail.com>
2014-10-30 11:46:37 +0100

    Test for correct solution in 34.

--- src/solutions/problem34.rs
+++ src/solutions/problem34.rs
@@ -11,3 +11,11 @@ fn factorial(n: int) -> int {
 pub fn solution() -> SolutionResult {
     Ok(range(3i, 2540160).filter(|&x| x == digits(x).into_iter().map(factorial).sum()).sum())
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn test_solution() {
+        assert_eq!(super::solution().map(|s| s % 100i), Ok(30));
+    }
+}
