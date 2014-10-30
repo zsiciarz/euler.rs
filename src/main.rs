a995e0d5f7bf1fce228d2a252cfc5be17dc7840b
Zbigniew Siciarz <antyqjon@gmail.com>
2014-10-30 14:26:52 +0100

    fail -> panic

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() {
     ];
     let matches = match getopts(args.tail(), opts) {
         Ok(m) => { m },
-        Err(f) => { fail!(f.to_string()) }
+        Err(f) => { panic!(f.to_string()) }
     };
     let problem_number = match matches.opt_str("p") {
         Some(p) => { p },
