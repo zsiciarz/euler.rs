e58b1c4a5b84b796e31529c772ae3291f6bab166
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-01-08 15:56:30 +0100

    Fixed 16.

--- src/solutions/problem16.rs
+++ src/solutions/problem16.rs
@@ -1,14 +1,13 @@
-use std::iter::AdditiveIterator;
 use std::num::{from_int, ToPrimitive};
-use num;
-use num::BigInt;
+use num::{self, BigInt, Zero};
 
 use super::{SolutionResult, SolutionError};
 use super::common::digits;
 
 pub fn solution() -> SolutionResult {
     let base: BigInt = from_int(2).unwrap();
-    match digits(num::pow(base, 1000)).into_iter().sum().to_int() {
+    let zero: BigInt = Zero::zero();
+    match digits(num::pow(base, 1000)).into_iter().fold(zero, |acc, x| acc + x).to_int() {
         Some(x) => Ok(x),
         None => Err(SolutionError::MatchFailed),
     }
