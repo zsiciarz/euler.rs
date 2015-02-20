f142782c7eb41545fe72ce0b316c2dd236d518ef
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-02-20 17:00:10 +0100

    Updated feature flags.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(collections, core, env, io, path, str_words, unicode)]
+#![feature(collections, core, env, old_io, old_path, str_words, unicode)]
 
 extern crate num;
 extern crate getopts;
