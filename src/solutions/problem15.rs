59e556336694fa19b5dc7b5e4e9786e1211741e0
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-10-25 20:02:31 +0100

    Remove redundant parentheses.

--- src/solutions/problem15.rs
+++ src/solutions/problem15.rs
@@ -3,7 +3,7 @@ use super::SolutionResult;
 
 pub fn solution() -> SolutionResult {
     let mut row = vec![1i64];
-    for _ in (0..40) {
+    for _ in 0..40 {
         let mut new_row = row.clone();
         new_row.insert(0, 0);
         row.push(0);
