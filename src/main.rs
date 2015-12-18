7db1aad08cde2e4bd14e9bd6d7544186828a5739
Zbigniew Siciarz <zbigniew@siciarz.net>
2015-12-18 12:37:41 +0100

    Run rustfmt on source code.

--- src/main.rs
+++ src/main.rs
@@ -17,31 +17,29 @@ fn main() {
     let mut opts = Options::new();
     opts.optopt("p", "problem", "Problem number", "PROBLEM NUMBER");
     let matches = match opts.parse(args) {
-        Ok(m) => {
-            m
-        }
-        Err(f) => {
-            panic!(f.to_string())
-        }
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
     };
     let problem_number = match matches.opt_str("p") {
-        Some(p) => {
-            p
-        }
+        Some(p) => p,
         None => {
             println!("Usage: euler --problem=<PROBLEM NUMBER>");
             return;
         }
     };
     match problem_number.parse::<isize>() {
-        Ok(problem_number) => match solutions::solution(problem_number) {
-            Ok(x) => println!("{}", x),
-            Err(e) => match e {
-                SolutionError::NotImplemented => println!("Not implemented yet"),
-                SolutionError::SolutionNotFound => println!("Solution not found"),
-                SolutionError::MatchFailed => println!("Pattern matching failed"),
-            },
-        },
+        Ok(problem_number) => {
+            match solutions::solution(problem_number) {
+                Ok(x) => println!("{}", x),
+                Err(e) => {
+                    match e {
+                        SolutionError::NotImplemented => println!("Not implemented yet"),
+                        SolutionError::SolutionNotFound => println!("Solution not found"),
+                        SolutionError::MatchFailed => println!("Pattern matching failed"),
+                    }
+                }
+            }
+        }
         Err(_) => println!("Invalid problem number"),
     }
 }
